6c4dccf9799e62b299cf08b478a42b2c0193fc42
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-23 07:50:31 -0700

    puching updates

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# this create a file in /tmp, using Puppet.
+
 file { '/tmp/shcool':
-ensure  => present,
-mode    => '0744',
-group   => 'www-data',
-owner   => 'www-data',
-content => 'I love Puppet',
+  ensure  => 'present',
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data',
+  content => 'I love Puppet',
 }
