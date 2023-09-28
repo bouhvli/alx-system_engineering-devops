6f4cddf84d73652982a251c90da877574a44d1fb
bouhvli <bouhali.hamza.2@gmail.com>
2023-09-28 04:21:15 -0700

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,6 +2,5 @@
 # takes, sends, and displays the size of the body of the response.
 # taking the URL
 URL_GIVEN=$1
-echo $URL_GIVEN
 # sends the request to the url
 curl -w "%{size_download}\n" $URL_GIVEN
