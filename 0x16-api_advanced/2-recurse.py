fcf8e9977d05502a26a319a79e9f9c0ae5559676
bouhvli <bouhali.hamza.2@gmail.com>
2023-11-08 00:53:53 +0100

    fisrt commit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,20 +11,17 @@ def recurse(subreddit, hot_list=[], after=""):
     if not subreddit or subreddit == "":
         return None
     url = "https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'bot/1.0 (by /u/bouhvli)'}
+    headers = {'User-Agent': 'Custom)'}
     response = requests.get(url, headers=headers, params={"after": after})
 
     if response.status_code == 200:
-        try:
-            data = response.json()
-            for post in data['data']['children']:
-                hot_list.append(post['data']['title'])
-            after = data['data']['after']
-            if after is None:
-                return hot_list
-            else:
-                return recurse(subreddit, hot_list, after)
-        except(KeyError):
-            return None
+        data = response.json()
+        for post in data['data']['children']:
+            hot_list.append(post['data']['title'])
+        after = data['data']['after']
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
     else:
         return None
