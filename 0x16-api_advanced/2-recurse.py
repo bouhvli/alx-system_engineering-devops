4983199ab114ef650242592f52596f080798c16f
bouhvli <bouhali.hamza.2@gmail.com>
2023-11-08 00:57:26 +0100

    fisrt commit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """sumary_line
 """
+
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
     """sumary_line
     """
-    
-    if not subreddit or subreddit == "":
-        return None
+
     url = "https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'Custom)'}
     response = requests.get(url, headers=headers, params={"after": after})
