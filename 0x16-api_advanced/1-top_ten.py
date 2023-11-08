96835145e7a0a2a9e97466f3628b1012a70cb13d
bouhvli <bouhali.hamza.2@gmail.com>
2023-11-08 01:01:58 +0100

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,18 +8,15 @@ def top_ten(subreddit):
     """sumary_line
     """
     
-    url = "https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'MyBot/1.0 (by /u/bouhvli)'}
+    url = "https://api.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Bouhvli)'}
+    response = requests.get(url, headers=headers, params={"limit": 10})
 
-    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        try:
-            data = response.json()
-            posts = data['data']['children']
-            for i, post in enumerate(posts):
-                title = post['data']['title']
-                print("{}. {}".format(i + 1 - 1, title))
-        except(KeyError):
-            print("None")
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            title = post['data']['title']
+            print(title)
     else:
         print("None")
