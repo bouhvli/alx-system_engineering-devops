97049a4b1d11700b4d09f1fdc6784527c8f76a5f
bouhvli <bouhali.hamza.2@gmail.com>
2023-11-08 01:10:06 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,9 @@ import requests
 def number_of_subscribers(subreddit):
     """sumary_line
     """
-    
+    headers = {'User-Agent': 'Custom)'}
     url = "https://api.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False)
+    response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
         res = response.json()["data"]["subscribers"]
         return res
