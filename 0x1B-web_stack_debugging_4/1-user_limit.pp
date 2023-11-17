bb4c262bd1b9bd0dc739012eebec6724a0ef2ffe
bouhvli <bouhali.hamza.2@gmail.com>
2023-11-17 12:14:29 +0100

    first commit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,2 @@
-# Sky is the limit, let's bring that limit higher
-exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
--> exec { '/usr/bin/env service nginx restart': }
+# User limit
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
